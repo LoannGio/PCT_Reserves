4f03a074c662110af857dcbb799d1f841088aab3
Loann Giovannangeli <loann.giovannangeli@etu.u-bordeaux.fr>
2017-03-02 15:37:07 +0100

    first local app commit

---
+++ Reserves_sol/Reserves_sol/Models.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reserves_sol
+{
+    public class OeuvreSondage
+    {
+        public string titre;
+        public string auteur;
+        public string description;
+        public string url_image;
+        public int nbVotes;
+    }
+
+    public class Sondage
+    {
+        public string titre;
+        public string description;
+        public DateTime dateDebut;
+        public DateTime dateFin;
+        public List<OeuvreSondage> l_OeuvresSondages;
+    }
+}
